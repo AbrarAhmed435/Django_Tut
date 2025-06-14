9fb4b00c010a9dd1922b3662847cb8d8367f11fb
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-06-14 22:28:31 +0530

    update

--- Django/16_DRF/10_JWT/drf_course/drf_course/settings.py
+++ Django/16_DRF/10_JWT/drf_course/drf_course/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,4 +134,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=20),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
