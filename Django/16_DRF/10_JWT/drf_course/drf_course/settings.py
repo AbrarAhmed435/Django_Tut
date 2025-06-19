262766bcc7a348af697c21c812933fb7516b925e
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-06-19 16:59:49 +0530

    update

--- Django/16_DRF/10_JWT/drf_course/drf_course/settings.py
+++ Django/16_DRF/10_JWT/drf_course/drf_course/settings.py
@@ -137,6 +137,6 @@ REST_FRAMEWORK = {
 }
 """ https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html """
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=50),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
